a556087379fa45b3a956e6a72b7fa56df99bddfc
MrQui <kathistbklemm@gmail.com>
2023-09-30 20:44:06 +0200

    Finishing DB Using

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -13,7 +13,7 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
 def create_user(db: Session, user: schemas.UserCreate):
-    fake_hashed_password = user.password + "notreallyhashed"
+    fake_hashed_password = user.password
     db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
     db.add(db_user)
     db.commit()
--- sql_app/main.py
+++ sql_app/main.py
@@ -48,12 +48,6 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: 
 
     return {"access_token": user.email, "token_type": "bearer"}
 
-
-@app.get("/users/me")
-async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
-    print(current_user)
-    return current_user
-
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
@@ -61,22 +55,24 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
 
+@app.post("/users/{user_id}/passwords/", response_model=schemas.Password)
+def create_password_for_user(current_user: Annotated[User, Depends(get_current_user)], password: schemas.PasswordCreate, db: Session = Depends(get_db)):
+    user = crud.get_user_by_email(db, email=str(current_user))
+    return crud.create_user_password(db=db, password=password, user_id=user.id)
 
+
+@app.get("/passwords/", response_model=list[schemas.Password])
+def read_passwords(current_user: Annotated[User, Depends(get_current_user)], password_name: str, db: Session = Depends(get_db)):
+    user = crud.get_user_by_email(db, email=str(current_user))
+    password = crud.get_passwords(db, password_name=password_name, user_id=user.id)
+    return password
+
+
+'''
 @app.get("/users/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
-
-
-@app.post("/users/{user_id}/passwords/", response_model=schemas.Password)
-def create_password_for_user(current_user: Annotated[User, Depends(get_current_user)], user_id: int, password: schemas.PasswordCreate, db: Session = Depends(get_db)):
-    return crud.create_user_password(db=db, password=password, user_id=user_id)
-
-
-@app.get("/passwords/", response_model=list[schemas.Password])
-def read_passwords(current_user: Annotated[User, Depends(get_current_user)], password_name: str, db: Session = Depends(get_db)):
-    user = crud.get_user_by_email(db, email=current_user)
-    password = crud.get_passwords(db, password_name=password_name, user_id=user.id)
-    return password
+'''
