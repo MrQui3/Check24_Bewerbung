f9fe85c9e7792671233e2de0ba037eb9c4e53fce
MrQui <kathistbklemm@gmail.com>
2023-10-07 15:12:01 +0200

    Testing: AES CBC

--- sql_app/testing.py
+++ sql_app/testing.py
@@ -1,17 +1,24 @@
 from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad
+import json
+from base64 import b64decode, b64encode
 
-key = b'Sixteen byte key'
-cipher = AES.new(key, AES.MODE_EAX)
+from Crypto.Util.Padding import unpad
 
-password = "password"
+password = b"password"
+key = get_random_bytes(16)
+cipher = AES.new(key, AES.MODE_CBC)
+ct_bytes = cipher.encrypt(pad(password, AES.block_size))
+iv = b64encode(cipher.iv).decode('utf-8')
+ct = b64encode(ct_bytes).decode('utf-8')
+result = json.dumps({'iv': iv, 'ciphertext': ct})
+print(type(iv))
 
-nonce = cipher.nonce
-ciphertext, tag = cipher.encrypt_and_digest(password.encode("utf-8"))
-
-cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
-plaintext = cipher.decrypt(ciphertext)
-try:
-    cipher.verify(tag)
-    print("The message is authentic:", plaintext.decode("utf-8"))
-except ValueError:
-    print("Key incorrect or message corrupted")
+b64 = json.loads(result)
+iv = b64decode(b64['iv'])
+print(type(iv))
+ct = b64decode(b64['ciphertext'])
+cipher = AES.new(key, AES.MODE_CBC, iv)
+pt = unpad(cipher.decrypt(ct), AES.block_size)
+print(pt.decode('utf-8'))
